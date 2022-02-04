5372a8e8bdaa15e1a43108a188d925ee706cd1b0
Sebastiano <sebastiano.gaudeano@live.com>
2022-02-04 17:45:46 +0100

    advanced json configuration

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,24 +1,11 @@
-import 'dart:convert';
-
 import 'package:cli/cli.dart' as cli;
-import 'dart:io';
-
 import 'package:cli/configuration_provider.dart';
-
-
-// void main(List<String> arguments) async {
-//   final file = File('./apiKey.txt');
-//   final String apiKey = file.readAsStringSync();
-//   var quote =
-//       await cli.getQuote(apiKey: apiKey, character: 'Legolas', details: true);
-//   print('$quote!');
-// }
 
 void main(List<String> arguments) async {
   var provider = ConfigurationProvider(path: './apiKey.json');
-  final config = await provider.getConfigurationAsync();
-  
-  // var quote =
-  //     await cli.getQuote(apiKey: mapApiKey['ApiKey']!, character: 'Legolas', details: true);
-  // print('$quote!');
+  final c = provider.getConfigurationAsync();
+  final config = await c;
+  var quote =
+      await cli.getQuote(apiKey: config.apiKey, character: 'Legolas', details: true);
+  print('$quote!');
 }
