25a18bdfd774875d9d6eda6804b2aeb8b63956b1
Sebastiano <sebastiano.gaudeano@live.com>
2022-02-04 15:32:07 +0100

    commenti feature

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,8 +1,10 @@
 import 'package:cli/cli.dart' as cli;
 
+// TODO: L'api key va messa in un file e letta dal file
+// è sbagliato averla chiodata a codice
 const String apiKey = '5hc0oyCyPJfPDrCR5CX4';
 
 void main(List<String> arguments) async {
-  var quote = await cli.getQuote(apiKey: apiKey);
+  var quote = await cli.getQuote(apiKey: apiKey, character: 'Legolas', details: true);
   print('$quote!');
 }
--- lib/cli.dart
+++ lib/cli.dart
@@ -2,9 +2,15 @@ import 'dart:math';
 
 import 'package:cli/quote_service.dart';
 
-Future<String> getQuote({required String apiKey}) {
-  var qs = QuoteService(apiKey: apiKey);
 
+//TODO: Se alla funzione viene passato il parametro 'character'
+// mi piacerebbe che venissa scelta una citazione di quel personaggio
+//TODO: se alla funzione viene passato true al parametro details
+//vanno stampati a video i dettagli della citazione come "Film e Personaggio"
+Future<String> getQuote(
+    {required String apiKey, String? character, bool? details}) {
+  
+  var qs = QuoteService(apiKey: apiKey);
   var limitTotal = 2390; // limite massimo di citazioni
   var rnd = Random(DateTime.now().millisecondsSinceEpoch);
   return qs.quote(limit: 1, offset: rnd.nextInt(limitTotal));
