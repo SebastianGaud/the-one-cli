c0af39f87a6ba0a9c2c02fbb2954973cea0fc1f7
Sebastiano <sebastiano.gaudeano@live.com>
2022-02-04 17:12:33 +0100

    character search

--- lib/cli.dart
+++ lib/cli.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cli/character_service.dart';
 import 'package:cli/quote_service.dart';
 
 
@@ -8,10 +9,25 @@ import 'package:cli/quote_service.dart';
 //TODO: se alla funzione viene passato true al parametro details
 //vanno stampati a video i dettagli della citazione come "Film e Personaggio"
 Future<String> getQuote(
-    {required String apiKey, String? character, bool? details}) {
+    {required String apiKey, String? character, bool? details}) async{
   
+  String? charId;
+  if (character != null) {
+    final charService = CharacterService(apiKey:  apiKey);
+    final char = await charService.searchCharacter(character);
+    charId = char['_id'];
+  }
+
+  late String quote;
   var qs = QuoteService(apiKey: apiKey);
-  var limitTotal = 2390; // limite massimo di citazioni
-  var rnd = Random(DateTime.now().millisecondsSinceEpoch);
-  return qs.quote(limit: 1, offset: rnd.nextInt(limitTotal));
+  
+  if (charId != null) {
+    quote = await qs.characterQuote(charId: charId, limit: 1, offset: 100);
+  } else {
+    var limitTotal = 2390; // limite massimo di citazioni
+    var rnd = Random(DateTime.now().millisecondsSinceEpoch);
+    quote = await qs.quote(limit: 1, offset: rnd.nextInt(limitTotal));
+  }
+
+  return quote;
 }
